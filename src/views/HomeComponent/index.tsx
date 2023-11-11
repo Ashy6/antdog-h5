85b4c0139fb28c9a4141f92844d6265422b290ff
Your zengjunxiao <z1801273437@163.com>
2023-11-11 13:45:31 +0800

    feat: 提交临时路由入口

--- src/views/HomeComponent/index.tsx
+++ src/views/HomeComponent/index.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react"
-import NavBarComponent from "../../components/NavBarComponent"
-import SideBarComponent from "../SideBarComponent"
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'antd-mobile'
+import NavBarComponent from '../../components/NavBarComponent'
+import SideBarComponent from '../SideBarComponent'
+import routes from '../../routes'
 
 export default function Home() {
     const [visible, setVisible] = useState<boolean>(false)
 
+    const navigate = useNavigate()
+
     return (
-        <div className="full-100">
+        <div className='full-100'>
             <NavBarComponent sideBarOpenCallback={() => setVisible(true)}>
-                <SideBarComponent visible={visible} sideBarCloseCallback={() => setVisible(false)} />
+                <SideBarComponent
+                    visible={visible}
+                    sideBarCloseCallback={() => setVisible(false)}
+                />
             </NavBarComponent>
             首页
+            {/* TODO：临时页面路由： */}
+            {routes.map(item => (
+                <div key={item.path}>
+                    <Button onClick={() => navigate(item.path)}>{item.name}</Button>
+                </div>
+            ))}
         </div>
     )
 }
