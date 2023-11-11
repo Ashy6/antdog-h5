246705bbe4eb87ac21b6a6bd0a1f913ba5044b2e
ajie <m18210870360@163.com>
2023-11-11 21:59:49 +0800

    feat: 接入请求拦截器和下单组件部分 (#5)
    * feat: 接入请求拦截器和下单组件部分

---
+++ src/api/util.ts
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from 'axios';
+import { Codes } from './code';
+
+// export const API_URL = 'https://www.bee456.com'
+// export const API_URL = 'https://2167d872c4.zicp.fun'
+export const API_URL = '127.0.0.1:9001'
+// export const API_URL = '154.212.145.170:9001'
+
+export const API_PREFIX = '/api/gcard'
+
+export const instance = axios.create({
+    // baseURL: API_URL,
+    timeout: 40000
+})
+
+// 请求拦截器
+instance.interceptors.request.use(async (config: any) => {
+    // 判断token是否过期
+    const token = localStorage.getItem('AntdogToken')
+    if (token) {
+        config.headers.token = token
+    }
+    return config
+})
+
+export interface LoginParams {
+    mail: string
+    password: string
+}
+
+export const login = (options: LoginParams): Promise<RequestDate> => {
+    return instance({
+        url: `${API_PREFIX}/user/login`,
+        method: 'post',
+        data: options
+    })
+}
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+    response => {
+        // 在这里对返回的响应进行判断和处理
+        if (response.status === 200) {
+            // 响应状态码为200时，进行一些操作
+            if (response.data.code === Codes.ok) {
+                // 响应成功后的操作
+                return response.data
+            }
+            if (
+                [Codes.notLogin, Codes.unauthorized, Codes.tokenError].includes(
+                    response.data.code
+                )
+            ) {
+                // 这个项目采用接口请求返回状态码来判断路由
+                // window.location.href = LOGIN_URL
+                login({ mail: "13333623253@163.com", password: "123456" }).then(response => {
+                    if (response.data?.token) {
+                        localStorage.setItem('AntdogToken', response.data?.token);
+                    }
+                });
+                return Promise.reject();
+            }
+        } else {
+            // 响应状态码不为200时，进行其他操作
+            console.error('请求失败')
+        }
+        return response.data
+    },
+    error => {
+        // 对请求错误进行处理
+        console.error('请求出错', error)
+        return Promise.reject(error)
+    }
+)
+
+export interface ResponseDate {
+    records?: AnyObject[]
+    msg?: string
+    [key: string]: AnyObject | any
+}
+
+export interface RequestDate {
+    code?: number
+    data: ResponseDate
+    msg?: string
+    result?: boolean
+    timestamp?: number
+    tracedId?: string
+}
+
+export interface AxiosResponse {
+    config: AnyObject
+    data: RequestDate
+    header: AnyObject
+    request: XMLHttpRequest
+    status: number
+    statusText: string
+}
---
+++ src/views/OrderComponent/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Avatar, Image, Divider, } from 'antd-mobile';
+import messageIcon from '../../assets/png/message.png';
+import { AntOutline, SoundOutline } from 'antd-mobile-icons'
+
+import { getCardList } from '../../api/card';
+import "./order.scss";
+
+export const OrderComponent = (props: { data: any }) => {
+
+    const whatappUrl = 'http://baidu.com';
+
+    const avatar = 'https://images.unsplash.com/photo-1548532928-b34e3be62fc6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ';
+    const points = 1000;
+    const rate = 784.71;
+
+    useEffect(() => {
+        getCardList().then(response => {
+            // TODO: props 传进来后，删除接口调用 
+        });
+    }, [])
+
+    return <div className='order pl-12 pr-12'>
+        <div className='d-flex justify-content-between'>
+            <div className='d-flex align-items-center'>
+                <div>
+                    <Avatar src={avatar} />
+                    <div style={{ textAlign: 'center' }}>Youka</div>
+                </div>
+                <div className='text-align-left' style={{ marginLeft: '16rem' }}>
+                    <div>Group:Game Card</div>
+                    <div style={{ fontWeight: 'bold', color: '#fa8c16' }}>Deposit {points} Points</div>
+                </div>
+            </div>
+            <div onClick={() => window.open(whatappUrl)}>
+                <Image src={messageIcon} height='56rem' width='80rem' />
+            </div>
+        </div>
+        <Divider />
+        <div className='pl-8 pr-8'>
+            <div className='d-flex' style={{ fontWeight: 'bold', paddingBottom: '4rem' }}>
+                <div style={{ color: '#2f54eb', marginRight: '8rem' }}><SoundOutline style={{ marginRight: '2rem' }} /><span style={{ fontSize: '13rem' }}>Notice</span></div>
+                <div style={{ color: '#fa8c16' }}>1 Point{rate}NGN(Current quotes)</div>
+            </div>
+            <div className='text-align-left'>1. Your card price auto calculate by below forms.don't waste time ask "how much?</div>
+            <div className='text-align-left'>2. Click "Send More Cards" to paste all cards once.only form can lock price</div>
+            <div className='text-align-left'>3. Chat use for interactive.Submit this form is onl way to trade.Your card redeem byAntdog intelligent machine.</div>
+        </div>
+        <Divider />
+    </div>
+}
