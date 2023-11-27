80f8ffb7805d932108993d94327dcc9d282e1bfa
ajie <m18210870360@163.com>
2023-11-27 22:18:27 +0800

    optimze: 添加协商弹窗 (#10)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -6,6 +6,6 @@ export const formatTime = (createTime: string) => {
     const day = date.getDate()
     const hours = date.getHours()
     const minutes = date.getMinutes()
-    const formattedDate = `${year}.${month}.${day} ${hours}:${minutes}`
+    const formattedDate = `${year}.${month}.${day} ${hours}:${minutes < 10 ? `0${minutes}` : minutes}`
     return formattedDate
 }
